0cc02eb5770bf10288861340c6645555f73d915e
Natan Felles <natanfelles@gmail.com>
2017-11-29 01:33:03 -0200

    Remove decimals of bytes size format

--- src/functions.php
+++ src/functions.php
@@ -20,7 +20,7 @@ $function_size_conversion = function ($size) {
 		'YB',
 	];
 	$power = $size > 0 ? floor(log($size, 1024)) : 0;
-	return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
+	return number_format($size / pow(1024, $power), $power ? 2 : 0, '.', ',') . ' ' . $units[$power];
 };
 
 /**
