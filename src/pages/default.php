a6cc9ed557440a4839d3c184a472877595ec72e7
Natan Felles <natanfelles@gmail.com>
2018-06-12 07:10:32 -0300

    Allow to show POSIX user and group

--- src/pages/default.php
+++ src/pages/default.php
@@ -26,15 +26,28 @@
 			<td colspan="6">This directory is empty.</td>
 		</tr>
 <?php endif ?>
+<?php
+$posix = function_exists('posix_getpwuid') && function_exists('posix_getgrgid');
+?>
 <?php foreach($paths as $path): ?>
+<?php
+	$user = $group = '';
+	if ($posix)
+	{
+		$user = posix_getpwuid($path['owner']);
+		$user = $user['name'] . ($user['gecos'] ? ' - '. $user['gecos'] : '');
+
+		$group = posix_getgrgid($path['group'])['name'];
+	}
+?>
 		<tr>
 			<td>
 				<span><?= $path['type'] ?></span>
 				<a href="<?= $path['href'] ?>"><?= $path['filename'] ?></a>
 			</td>
 			<td><?= $path['size'] ?></td>
-			<td><?= $path['owner'] ?></td>
-			<td><?= $path['group'] ?></td>
+			<td title="<?= $user ?>"><?= $path['owner'] ?></td>
+			<td title="<?= $group ?>"><?= $path['group'] ?></td>
 			<td><?= $path['perms'] ?></td>
 			<td><?= $path['mTime'] ?></td>
 		</tr>
